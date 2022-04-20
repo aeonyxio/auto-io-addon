a1d2edf7928a4a48db2597bc66ae42f3b4be39c3
Tonks <russell.tonks@accenture.com>
2022-04-20 21:05:13 +0100

    clean

--- upload.ts
+++
@@ -1,8 +0,0 @@
-import { execSync } from "child_process";
-import { config } from "dotenv";
-
-config();
-
-const command = `prebuild --backend cmake-js --runtime napi --all --upload ${process.env.GITHUB_TOKEN}`;
-
-execSync(command);
